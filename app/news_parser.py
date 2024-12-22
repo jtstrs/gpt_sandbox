8b03713c8ee1fe95368a0c777a532e5d2b05e165
Script <bigalex934@gmail.com>
2024-12-22 18:23:29 +0300

    Implement base class for news parser

---
+++ app/news_parser.py
@@ -0,0 +1,7 @@
+class News:
+    pass
+
+class NewsParser:
+
+    def update_news() -> list[News]:
+        raise RuntimeError("Couldnot execute abstract function")
---
+++ app/telegram_channels_parser.py
@@ -0,0 +1,9 @@
+from news_parser import NewsParser, News
+
+class TelegramChannelsParser(NewsParser):
+
+    def __init__(self):
+        pass
+
+    def update_news() -> list[News]:
+        pass
