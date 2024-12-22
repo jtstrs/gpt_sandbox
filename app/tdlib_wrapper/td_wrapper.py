4d0f04b4574201f96b3ea50b2785b230699b574b
Script <bigalex934@gmail.com>
2024-12-22 20:57:12 +0300

    Pass tdlib to python

--- app/tdlib_wrapper/td_wrapper.py
+++ app/tdlib_wrapper/td_wrapper.py
@@ -1,7 +1,46 @@
 import ctypes
-import os
+
+# void *td_json_client_create_wrapper();
+# void td_json_client_send_wrapper(void *client, const char *request);
+# const char *td_json_client_receive_wrapper(void *client, double timeout);
+# const char *td_json_client_execute_wrapper(void *client, const char *request);
+# void td_json_client_destroy_wrapper(void *client);
 
 td_lib_dll = ctypes.CDLL("libs/libTDLibWrapper.so")
 
-def create_client_id():
-    td_lib_dll.td_create_client_id_wrapper()
+td_lib_dll.td_create_client_id_wrapper.restype = ctypes.c_int32
+
+td_lib_dll.td_send_wrapper.argtypes = [ctypes.c_int32, ctypes.c_char_p]
+
+td_lib_dll.td_receive_wrapper.restype = ctypes.c_char_p
+td_lib_dll.td_receive_wrapper.argtypes = [ctypes.c_double]
+
+td_lib_dll.td_execute_wrapper.restype = ctypes.c_char_p
+td_lib_dll.td_execute_wrapper.argtypes = [ctypes.c_char_p]
+
+td_lib_dll.td_json_client_create_wrapper.restype = ctypes.c_void_p
+
+td_lib_dll.td_json_client_destroy_wrapper.argtypes = [ctypes.c_void_p]
+
+
+def td_create_client_id() -> int:
+    return td_lib_dll.td_create_client_id_wrapper()
+
+def td_send(client_id: int, request: str):
+    td_lib_dll.td_send_wrapper(client_id, request)
+
+def td_receive(timeout: float) -> str:
+    td_lib_dll.td_receive_wrapper(timeout)
+
+def td_execute(request: str) -> str:
+    td_lib_dll.td_execute_wrapper(request)
+
+def td_set_log_message_callback(max_verbosity_level: int, callback):
+    td_lib_dll.td_set_log_message_callback_wrapper(max_verbosity_level, callback)
+
+def td_json_client_create():
+    return td_lib_dll.td_json_client_create_wrapper()
+
+def td_json_client_destroy(client):
+    td_lib_dll.td_json_client_destroy_wrapper(client)
+
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from app.tdlib_wrapper.td_wrapper import create_client_id
+from app.tdlib_wrapper.td_wrapper import td_json_client_create, td_json_client_destroy
 
 def main():
-    create_client_id()
+    client = td_json_client_create()
+    td_json_client_destroy(client)
+
+main()
